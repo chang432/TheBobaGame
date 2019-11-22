0a8418679eb56a34476124fd4347211f420a9327
andre <chang432@purdue.edu>
2019-11-21 20:14:38 -0500

    real initial commit

---
+++ Assets/Scripts/LoginScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginScript : MonoBehaviour
+{
+
+	public GameObject username;
+	public GameObject password;
+	public GameObject login;
+
+	public Button loginBtn;
+
+	private string Username;
+	private string Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Username = username.GetComponent<InputField> ().text;
+    	Password = password.GetComponent<InputField> ().text;
+
+    	loginBtn = login.GetComponent<Button>();
+    	loginBtn.onClick.AddListener(validateLogin);
+    }
+
+    void validateLogin() {
+    	print("WHATS UP BOIII");
+    	SceneManager.LoadScene(1);
+    }
+
+}
