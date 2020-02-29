6956dcbb5707d2ea90977c17bfe294d00626e886
bsung03 <bsung@purdue.edu>
2020-02-29 17:24:27 -0500

    game manager script

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+
+    public static int money;
+
+    public static string username;
+
+    public void setMoney(int value)
+    {
+        money = value;
+    }
+
+    public int getMoney()
+    {
+        return money;
+    }
+
+    public string getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(string s)
+    {
+        username = s;
+    }
+}
