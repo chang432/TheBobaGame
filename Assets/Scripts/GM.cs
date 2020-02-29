696fe2ad74889db57cf47b0f629e670b05b3e11f
bsung03 <bsung@purdue.edu>
2020-02-29 17:52:25 -0500

    Game manager prefab and script

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int money;
+
+    public void setMoney(int value)
+    {
+        money = value;
+    }
+
+    public int getMoney()
+    {
+        return money;
+    }
+}
