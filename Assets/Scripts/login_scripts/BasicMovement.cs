23af2d66a6e4cdebc21a21a73988848e028c56e4
andre <chang432@purdue.edu>
2020-02-29 16:58:03 -0500

    adding login branch functionality

---
+++ Assets/Scripts/login_scripts/BasicMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame 
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 newPos = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        newPos = newPos * 3 * Time.deltaTime;
+        rb.MovePosition(rb.transform.position + newPos);
+        /*Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * 3 * Time.deltaTime;
+
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * 3 * Time.deltaTime;*/
+    }
+}
---
+++ Assets/Scripts/login_scripts/DoneBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoneBtn : MonoBehaviour
+{
+    Patrol p;
+    GameObject customer1;
+    private void OnMouseDown()
+    {
+        customer1 = GameObject.Find("Customer1");
+        p = customer1.GetComponent<Patrol>();
+
+        if (p.animator.GetFloat("VerticalIdle") != 0)
+        {
+            p.animator.SetFloat("VerticalIdle", 0);
+            p.moving.x = -1;
+            p.moving.y = 0;
+        }
+    }
+}
