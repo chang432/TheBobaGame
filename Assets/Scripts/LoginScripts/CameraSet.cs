0e2eae2271cfeda65d2652b414137b1b4cff49f9
andre <chang432@purdue.edu>
2020-04-14 01:27:33 -0400

    fix camera position

---
+++ Assets/Scripts/LoginScripts/CameraSet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSet : MonoBehaviour
+{
+    public GameObject shopBackground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = shopBackground.GetComponent<SpriteRenderer>().bounds.size.y;
+        //print("height: " + height);
+        Camera.main.orthographicSize = height / 2;
+    }
+}
